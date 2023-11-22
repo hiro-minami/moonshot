5b8df85e4bcbafed5e73149e7c04bf568748d044
hiro-minami <hiro.0907@icloud.com>
2023-11-22 23:24:13 +0900

    Add Radix UI theme and background color to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+import "@radix-ui/themes/styles.css";
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,10 +25,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider cookies={cookies().toString()}>
-          {children}
-        </TRPCReactProvider>
+      <body className={`bg-[#fffafa] font-sans ${inter.variable}`}>
+        <Theme>
+          <TRPCReactProvider cookies={cookies().toString()}>
+            {children}
+          </TRPCReactProvider>
+        </Theme>
       </body>
     </html>
   );
