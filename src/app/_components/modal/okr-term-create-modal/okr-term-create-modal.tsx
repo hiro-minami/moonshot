27cf5df86a1f49233d6bbba7b4db40e8d49f6d1f
hiro-minami <hiro.0907@icloud.com>
2023-11-25 16:23:33 +0900

    feat(modal): add OKR create modal footer and title components
    - Added a new file `okr-create-modal-footer.tsx` that contains the
    footer component for the OKR create modal.
    - Added a new file `okr-create-modal-title.tsx` that contains the title
    component for the OKR create modal.
    - Refactored the existing `okr-term-create-modal.tsx` file to use the
    new footer and title components.
    - Removed the duplicate code for the title and footer from the content
    section of the modal.
    - Passed the children prop to the `OkrTermCreateModal` component to
    render the dynamic content within the modal.
    
    This commit improves the code structure and reusability of the OKR term
    create modal.

--- src/app/_components/modal/okr-term-create-modal/okr-term-create-modal.tsx
+++ src/app/_components/modal/okr-term-create-modal/okr-term-create-modal.tsx
@@ -1,8 +1,13 @@
-import { Button, Dialog } from "@radix-ui/themes";
-import { Cross2Icon } from "@radix-ui/react-icons";
+"use client";
 
-export const OkrTermCreateModal = () => {
-  // TODO: RHFとZodを使ってバリデーションを実装する
+import { Button, Dialog } from "@radix-ui/themes";
+import { OkrCreateModalTitle } from "./okr-create-modal-title";
+
+type OkrTermCreateModalProps = {
+  children: React.ReactNode;
+};
+
+export const OkrTermCreateModal = ({ children }: OkrTermCreateModalProps) => {
   return (
     <Dialog.Root>
       <Dialog.Trigger>
@@ -12,57 +17,9 @@ export const OkrTermCreateModal = () => {
       </Dialog.Trigger>
       <Dialog.Content>
         <Dialog.Title>
-          <div className="flex flex-row justify-between">
-            <span>OKR期間の作成</span>
-            <Dialog.Close>
-              <Button className="text-[#d3d3d3] hover:text-black">
-                <Cross2Icon />
-              </Button>
-            </Dialog.Close>
-          </div>
+          <OkrCreateModalTitle />
         </Dialog.Title>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-1">
-            <label className="text-sm" htmlFor="name">
-              期間名
-            </label>
-            <input
-              className="h-[30px] w-[100%] border-b-2 outline-0 focus-within:border-[#9f53ec]"
-              id="name"
-              placeholder="OKR期間1"
-            />
-          </div>
-          <fieldset className="flex flex-col gap-1">
-            <label className="text-sm" htmlFor="start-date">
-              期間
-            </label>
-            <div className="flex flex-row gap-4">
-              <input
-                type="date"
-                className="h-[30px] w-[100%] border-b-2 outline-0 focus-within:border-[#9f53ec]"
-                id="start-date"
-                placeholder="@peduarte"
-              />
-              <span> ~ </span>
-              <input
-                type="date"
-                className="h-[30px] w-[100%] border-b-2 outline-0 focus-within:border-[#9f53ec]"
-                id="start-date"
-                placeholder="@peduarte"
-              />
-            </div>
-          </fieldset>
-        </div>
-        <div className="mt-4 flex flex-row justify-end gap-4">
-          <Dialog.Close>
-            <Button className="text-[#d3d3d3] hover:text-black">
-              キャンセル
-            </Button>
-          </Dialog.Close>
-          <Dialog.Close>
-            <Button className="bg-[#9f53ec]">作成</Button>
-          </Dialog.Close>
-        </div>
+        {children}
       </Dialog.Content>
     </Dialog.Root>
   );
