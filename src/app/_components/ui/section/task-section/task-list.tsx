ed7cdd8634ea9838823d1176306721cb3ddd00b4
hiro-minami <hiro.0907@icloud.com>
2023-12-13 19:11:09 +0900

    chore: Add TaskList component and refactor TaskSection

---
+++ src/app/_components/ui/section/task-section/task-list.tsx
@@ -0,0 +1,33 @@
+import { ScrollArea } from "@radix-ui/themes";
+import type { KeyResultWithTasks } from "~/types";
+import { TaskCreateForm } from "../../form/task-create-form";
+import { TaskItem } from "./task-item";
+
+type TaskListProps = {
+  keyResults: ReadonlyArray<KeyResultWithTasks>;
+  onClick: (taskId: number) => void;
+};
+
+export const TaskList = ({ keyResults, onClick }: TaskListProps) => {
+  const height = window.innerHeight - 180;
+  return (
+    <ScrollArea
+      type="always"
+      scrollbars="vertical"
+      className="mt-2 px-6 overflow-y-scroll"
+      style={{ height: height }}
+    >
+      {keyResults.map((keyResult) => (
+        <div key={keyResult.id} className="pb-4">
+          <span className="font-bold">{keyResult.name}</span>
+          <div className="flex flex-col gap-2">
+            {keyResult.tasks.map((task) => (
+              <TaskItem key={task.id} task={task} onClick={onClick} />
+            ))}
+            <TaskCreateForm keyResultId={keyResult.id} />
+          </div>
+        </div>
+      ))}
+    </ScrollArea>
+  );
+};
