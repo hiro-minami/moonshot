ddeb17cabd6f4de4c3861a6367011ccdea1299b4
hiro-minami <hiro.0907@icloud.com>
2023-12-21 20:44:32 +0900

    chore: update key result card component

--- src/app/_components/ui/card/key-result-card/key-result-card.tsx
+++ src/app/_components/ui/card/key-result-card/key-result-card.tsx
@@ -1,25 +1,26 @@
 import type { KeyResult } from "@prisma/client";
 import { TargetIcon } from "@radix-ui/react-icons";
-import { Box, Button, Card, Text, Tooltip } from "@radix-ui/themes";
-import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { Box, Card } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 import { api } from "~/trpc/react";
 
-import { CheckinModal } from "../../modal/check-in-modal";
 import { OptionButton } from "../_components";
+import { KeyResultName } from "./key-result-name";
+import { KeyResultProgressRate } from "./key-result-progress-rate";
 
 type KeyResultCardProps = {
   keyResult: KeyResult;
   progressRate: number;
+  readonly?: boolean;
 };
 
 export const KeyResultCard = ({
   keyResult,
   progressRate,
+  readonly = false,
 }: KeyResultCardProps) => {
   const router = useRouter();
-  const pathname = usePathname();
 
   const { mutate } = api.keyResult.deleteKeyResult.useMutation({
     onSuccess: () => {
@@ -37,31 +38,18 @@ export const KeyResultCard = ({
         <div className="flex flex-row items-center gap-[12px]">
           <TargetIcon className="w-[30px]" />
           <Box>
-            <Link
-              href={`${pathname}/key-result/${btoa(
-                `KeyResultId:${keyResult.id}`,
-              )}`}
-            >
-              <Text as="div" size="2" weight="bold">
-                {keyResult.name}
-              </Text>
-            </Link>
+            <KeyResultName
+              id={keyResult.id}
+              name={keyResult.name}
+              readonly={readonly}
+            />
           </Box>
         </div>
         <div className="flex flex-row items-center gap-4">
-          <CheckinModal keyResults={[keyResult]}>
-            <Button className="p-0 h-0">
-              <Tooltip content={`${progressRate}%`}>
-                <div className="h-2.5 w-[100px] rounded-full  bg-gray-200">
-                  <div
-                    className="h-full rounded-full bg-[#9f53ec]"
-                    style={{ width: `${progressRate}%` }}
-                  />
-                </div>
-              </Tooltip>
-            </Button>
-          </CheckinModal>
-          <OptionButton onClick={deleteKeyResult} keyResult={keyResult} />
+          <KeyResultProgressRate {...{ keyResult, progressRate, readonly }} />
+          {!readonly && (
+            <OptionButton onClick={deleteKeyResult} keyResult={keyResult} />
+          )}
         </div>
       </div>
     </Card>
