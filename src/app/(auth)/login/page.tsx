3c3914a7b41064649c00817ae11110c737767b46
hiro-minami <hiro.0907@icloud.com>
2023-12-28 00:05:16 +0900

    feat: Refactor login page to use Login component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import { getServerAuthSession } from "~/server/auth";
+import { Login } from "./login";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -11,16 +11,5 @@ export default async function Home() {
     return redirect(redirectUrl);
   }
 
-  return (
-    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-      <div className="flex flex-col items-center gap-2">
-        <Link
-          href={session ? "/api/auth/signout" : "/api/auth/signin"}
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-        >
-          {session ? "Sign out" : "Sign in"}
-        </Link>
-      </div>
-    </div>
-  );
+  return <Login session={session!} />;
 }
