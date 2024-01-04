413fec8c808441e4bddf1092b9f3c46b320102fa
hiro-minami <hiro.0907@icloud.com>
2024-01-04 21:32:20 +0900

    feat: Refactor page-content component and update task cards

--- src/app/okr/[okr-term-id]/key-result/[key-result-id]/page-content.tsx
+++ src/app/okr/[okr-term-id]/key-result/[key-result-id]/page-content.tsx
@@ -1,126 +1,53 @@
 "use client";
 
-import { Card, Grid, ScrollArea, Tooltip } from "@radix-ui/themes";
+import { Grid } from "@radix-ui/themes";
 import { Chart, registerables } from "chart.js";
-import { Line } from "react-chartjs-2";
+import { CompletedTaskCountCard } from "~/app/_components/ui/card/completed-task-count-card";
+import { CompletedTaskRateCard } from "~/app/_components/ui/card/completed-task-rate-card";
 import { KeyResultCard } from "~/app/_components/ui/card/key-result-card";
 import { TaskCard } from "~/app/_components/ui/card/task-card";
-import { TaskItem } from "~/app/_components/ui/section/task-section/task-item";
+import { TaskListCard } from "~/app/_components/ui/card/task-list-card";
+import { useTaskMetrics } from "~/hooks";
 import type { KeyResultWithTasks } from "~/types";
 
 type PageContentProps = {
   keyResult: KeyResultWithTasks;
 };
 
+Chart.register(...registerables);
+
 export const PageContent = ({ keyResult }: PageContentProps) => {
-  const taskCount = keyResult.tasks.length;
-  const finishTaskCount = keyResult.tasks.filter((task) => task.isDone).length;
-
-  const completedTasks = keyResult.tasks.filter(
-    (task) => task.isDone && task.endDate,
-  );
-
-  // TODO: 完了日がないタスクをどうするか考える
-  const tasksByDate = completedTasks.reduce(
-    (acc, task) => {
-      const date = task.endDate!.toLocaleDateString();
-      if (!acc[date]) {
-        acc[date] = [];
-      }
-      acc[date]!.push(task);
-
-      return acc;
-    },
-    {} as Record<string, typeof completedTasks>,
-  );
-
-  const taskCountsByDate = Object.entries(tasksByDate)
-    .map(([date, tasks]) => ({
-      date,
-      count: tasks.length,
-    }))
-    .sort((a, b) => (new Date(a.date) < new Date(b.date) ? -1 : 1));
-
-  const dates = taskCountsByDate.map((task) => task.date);
-  const counts = taskCountsByDate.map((task) => task.count);
-
-  Chart.register(...registerables);
-
-  const options = {
-    maintainAspectRatio: false,
-    responsive: false,
-    lineTension: 0.4,
-  };
-
-  const width = window.innerWidth;
-
-  const progressRate = Math.round(
-    (keyResult.currentValue / keyResult.targetValue) * 100,
-  );
+  const {
+    taskCount,
+    finishTaskCount,
+    dates,
+    counts,
+    progressRate,
+    unfinishedTaskCount,
+    taskCompletionRate,
+  } = useTaskMetrics(keyResult);
 
   return (
     <div className="flex flex-col gap-4">
-      <KeyResultCard {...{ keyResult, progressRate, readonly: true }} />
+      <KeyResultCard
+        keyResult={keyResult}
+        progressRate={progressRate}
+        readonly={true}
+      />
       <Grid columns="4" gap="4" className="w-[60%]">
         <TaskCard name="タスク数" count={taskCount} />
-        <TaskCard name="未完了のタスク" count={taskCount - finishTaskCount} />
+        <TaskCard name="未完了のタスク" count={unfinishedTaskCount} />
         <TaskCard name="完了したタスク" count={finishTaskCount} />
+        <TaskCard name="タスクの消化率" count={taskCompletionRate} unit="%" />
       </Grid>
       <div className="grid grid-cols-10 gap-3">
-        <Card className="flex flex-col gap-2 bg-white col-span-7">
-          <span>完了したタスク数の推移</span>
-          <Line
-            width={width / 1.6}
-            height={300}
-            data={{
-              labels: dates,
-              datasets: [
-                {
-                  label: "完了したタスク",
-                  data: counts,
-                  fill: false,
-                  backgroundColor: "#9f53ec",
-                  borderColor: "rgba(159, 83, 236, 0.2)",
-                },
-              ],
-            }}
-            options={options}
-          />
-        </Card>
-        <Card className="h-[348px] col-span-3">
-          <div className="flex flex-col gap-[8px] bg-white">
-            <span>タスクの消化率</span>
-            <Tooltip
-              content={`${Math.floor((finishTaskCount / taskCount) * 100)}%`}
-              delayDuration={100}
-            >
-              <div
-                className="flex justify-center items-center mr-auto ml-auto w-[280px] h-[280px] rounded-[50%] ml-[49px] mr-[49px]"
-                style={{
-                  backgroundImage: `radial-gradient(#f2f2f2 60%, transparent 61%), conic-gradient(#9f53ec 0% ${Math.floor(
-                    (finishTaskCount / taskCount) * 100,
-                  )}%, #d9d9d9 60% 100%)`,
-                }}
-              />
-            </Tooltip>
-          </div>
-        </Card>
+        <CompletedTaskCountCard dates={dates} counts={counts} />
+        <CompletedTaskRateCard
+          taskCount={taskCount}
+          finishTaskCount={finishTaskCount}
+        />
       </div>
-      <Card>
-        <ScrollArea
-          type="always"
-          scrollbars="vertical"
-          className="p-6 bg-white"
-        >
-          {keyResult.tasks.map((task) => (
-            <TaskItem
-              key={task.id}
-              task={task}
-              onClick={() => console.log("click")}
-            />
-          ))}
-        </ScrollArea>
-      </Card>
+      <TaskListCard keyResult={keyResult} />
     </div>
   );
 };
