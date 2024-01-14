08a9f8d03f6b46caa54e386edcea477d391b9ff6
hiro-minami <hiro.0907@icloud.com>
2024-01-15 07:48:24 +0900

    chore: Refactor KeyResultProgressRate component

--- src/app/_components/ui/card/key-result-card/key-result-progress-rate.tsx
+++ src/app/_components/ui/card/key-result-card/key-result-progress-rate.tsx
@@ -1,55 +1,27 @@
-import type { KeyResult } from "@prisma/client";
 import * as Progress from "@radix-ui/react-progress";
-import { Button, Tooltip } from "@radix-ui/themes";
-import { CheckinModal } from "../../modal/check-in-modal";
+import { Tooltip } from "@radix-ui/themes";
 
 type KeyResultProgressRateProps = {
-  keyResult: KeyResult;
   progressRate: number;
-  readonly: boolean;
 };
 
 export const KeyResultProgressRate = ({
-  keyResult,
   progressRate,
-  readonly,
 }: KeyResultProgressRateProps) => {
-  if (readonly) {
-    return (
-      <Tooltip content={`${progressRate}%`} delayDuration={100}>
-        <Progress.Root
-          className="relative bg-gray-200 overflow-hidden rounded-full w-[100px] h-2.5"
-          style={{
-            transform: "translateZ(0)",
-          }}
-          value={progressRate}
-        >
-          <Progress.Indicator
-            className="bg-[#9f53ec] w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
-            style={{ transform: `translateX(-${100 - progressRate}%)` }}
-          />
-        </Progress.Root>
-      </Tooltip>
-    );
-  }
   return (
-    <CheckinModal keyResults={[keyResult]}>
-      <Button className="p-0 h-0">
-        <Tooltip content={`${progressRate}%`} delayDuration={100}>
-          <Progress.Root
-            className="relative bg-gray-200 overflow-hidden rounded-full w-[100px] h-2.5"
-            style={{
-              transform: "translateZ(0)",
-            }}
-            value={progressRate}
-          >
-            <Progress.Indicator
-              className="bg-[#9f53ec] w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
-              style={{ transform: `translateX(-${100 - progressRate}%)` }}
-            />
-          </Progress.Root>
-        </Tooltip>
-      </Button>
-    </CheckinModal>
+    <Tooltip content={`進捗：${progressRate}%`} delayDuration={100}>
+      <Progress.Root
+        className="relative bg-gray-200 overflow-hidden rounded-full w-[100px] h-2.5"
+        style={{
+          transform: "translateZ(0)",
+        }}
+        value={progressRate}
+      >
+        <Progress.Indicator
+          className="bg-[#9f53ec] w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
+          style={{ transform: `translateX(-${100 - progressRate}%)` }}
+        />
+      </Progress.Root>
+    </Tooltip>
   );
 };
