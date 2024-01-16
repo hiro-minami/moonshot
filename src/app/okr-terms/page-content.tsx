f05b231cdc269484bf981f9695e8f3c6c1ddc37e
hiro-minami <hiro.0907@icloud.com>
2024-01-17 06:07:23 +0900

    chore: Refactor OkrTerm page layout

--- src/app/okr-terms/page-content.tsx
+++ src/app/okr-terms/page-content.tsx
@@ -15,29 +15,35 @@ export const PageContent = ({ okrTerms, createdById }: Props) => {
   const height = Math.floor(window.innerHeight * 0.655);
 
   return (
-    <>
-      <div className="align-center flex justify-between">
-        <span className="ml-6 text-[32px] font-bold">達成したい目標</span>
-        <OkrTermCreateModal>
-          <OkrTermCreateForm createdById={createdById} />
-        </OkrTermCreateModal>
+    <div className="flex justify-center">
+      <div className="max-w-[1200px]">
+        <div className="align-center flex justify-between">
+          <span className="ml-6 text-[32px] font-bold">達成したい目標</span>
+          <OkrTermCreateModal>
+            <OkrTermCreateForm createdById={createdById} />
+          </OkrTermCreateModal>
+        </div>
+        <ScrollArea
+          type="always"
+          scrollbars="vertical"
+          className="mt-8 px-6"
+          style={{ height }}
+        >
+          <div className="flex justify-center">
+            <div className="max-w-[900px]">
+              {okrTerms.length > 0 ? (
+                <Grid columns="3" gap="4" width="auto">
+                  {okrTerms.map((okrTerm) => (
+                    <OkrTermCard key={okrTerm.id} okrTerm={okrTerm} />
+                  ))}
+                </Grid>
+              ) : (
+                <div className="flex flex-col gap-4 pt-4">no Okr Terms</div>
+              )}
+            </div>
+          </div>
+        </ScrollArea>
       </div>
-      <ScrollArea
-        type="always"
-        scrollbars="vertical"
-        className="mt-8 px-6"
-        style={{ height }}
-      >
-        {okrTerms.length > 0 ? (
-          <Grid columns="4" gap="4" width="auto">
-            {okrTerms.map((okrTerm) => (
-              <OkrTermCard key={okrTerm.id} okrTerm={okrTerm} />
-            ))}
-          </Grid>
-        ) : (
-          <div className="flex flex-col gap-4 pt-4">no Okr Terms</div>
-        )}
-      </ScrollArea>
-    </>
+    </div>
   );
 };
