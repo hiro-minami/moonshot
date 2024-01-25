82250f5f7b473ce8f74f22fe3c51e95d3a7c9aac
hiro-minami <hiro.0907@icloud.com>
2024-01-25 09:43:58 +0900

    fix: Refactor page-content component layout

--- src/app/okr-terms/page-content.tsx
+++ src/app/okr-terms/page-content.tsx
@@ -15,35 +15,33 @@ export const PageContent = ({ okrTerms, createdById }: Props) => {
   const height = Math.floor(window.innerHeight * 0.655);
 
   return (
-    <div className="flex justify-center">
-      <div className="max-w-[1200px]">
-        <div className="align-center flex justify-between">
-          <span className="ml-6 text-[32px] font-bold">達成したい目標</span>
-          <OkrTermCreateModal>
-            <OkrTermCreateForm createdById={createdById} />
-          </OkrTermCreateModal>
-        </div>
-        <ScrollArea
-          type="always"
-          scrollbars="vertical"
-          className="mt-8 px-6"
-          style={{ height }}
-        >
-          <div className="flex justify-center">
-            <div className="max-w-[900px]">
-              {okrTerms.length > 0 ? (
-                <Grid columns="3" gap="4" width="auto">
-                  {okrTerms.map((okrTerm) => (
-                    <OkrTermCard key={okrTerm.id} okrTerm={okrTerm} />
-                  ))}
-                </Grid>
-              ) : (
-                <div className="flex flex-col gap-4 pt-4">no Okr Terms</div>
-              )}
-            </div>
-          </div>
-        </ScrollArea>
+    <div className="flex flex-col justify-center">
+      <div className="align-center flex justify-between">
+        <span className="ml-6 text-[32px] font-bold">達成したい目標</span>
+        <OkrTermCreateModal>
+          <OkrTermCreateForm createdById={createdById} />
+        </OkrTermCreateModal>
       </div>
+      <ScrollArea
+        type="always"
+        scrollbars="vertical"
+        className="mt-8 px-6"
+        style={{ height }}
+      >
+        <div className="flex justify-center">
+          <div className="max-w-[900px]">
+            {okrTerms.length > 0 ? (
+              <Grid columns="3" gap="4" width="auto">
+                {okrTerms.map((okrTerm) => (
+                  <OkrTermCard key={okrTerm.id} okrTerm={okrTerm} />
+                ))}
+              </Grid>
+            ) : (
+              <div className="flex flex-col gap-4 pt-4">no Okr Terms</div>
+            )}
+          </div>
+        </div>
+      </ScrollArea>
     </div>
   );
 };
